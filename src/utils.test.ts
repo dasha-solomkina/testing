52054dd1707ea06c40a13e71edad70eaa8204637
daria solomkina <solomkina.daria@gmail.com>
2025-09-12 07:41:14 -0300

    chore: fix import declarations

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -2,6 +2,8 @@ import double from './utils'
 
 describe('Numbers utils', () => {
   it('should double the number', () => {
+    expect(double(1)).toBe(2)
     expect(double(2)).toBe(4)
+    expect(double(3)).toBe(6)
   })
 })
